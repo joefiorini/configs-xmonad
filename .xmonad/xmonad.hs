62e60c3c035dcb15f5499d32ae3b9e33f3827756
Joe Fiorini <joe@joefiorini.com>
2014-05-18 20:17:24 -0400

    Set a few options

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,7 +1,82 @@
 import XMonad
+import XMonad.Layout.Decoration
+
+-- Solarized Colors
+base03  = "#002b36"
+base02  = "#073642"
+base01  = "#586e75"
+base00  = "#657b83"
+base0   = "#839496"
+base1   = "#93a1a1"
+base2   = "#eee8d5"
+base3   = "#fdf6e3"
+yellow  = "#b58900"
+orange  = "#cb4b16"
+red     = "#dc322f"
+magenta = "#d33682"
+violet  = "#6c71c4"
+blue    = "#268bd2"
+cyan    = "#2aa198"
+green   = "#719e07"
+
+myNormalBorderColor   = "#000000"
+myFocusedBorderColor  = base03
+myBorderWidth         = 0
+
+active = green
+
+baseTheme :: Theme
+baseTheme = defaultTheme
+    { activeColor           = base03
+    , activeBorderColor     = base03
+    , activeTextColor       = base01 -- blue also good
+    , inactiveBorderColor   = base02
+    , inactiveColor         = base02
+    , inactiveTextColor     = base01
+    , urgentColor           = yellow
+    , urgentBorderColor     = yellow
+    , urgentTextColor       = base02
+    -- , decoHeight            = 22
+    , decoHeight            = 20
+    }
+
+tabThemeDim :: Theme
+tabThemeDim = baseTheme
+    { -- base00, base01, blue all good activeColors
+      activeColor           = base03
+    , activeBorderColor     = base03
+    , activeTextColor       = base01
+    }
+
+tabTheme :: Theme
+tabTheme = baseTheme
+    { -- base00, base01, blue all good activeColors
+      activeColor           = active
+    , activeBorderColor     = base03 
+    , activeTextColor       = base03
+    }
+
+tileTheme :: Theme
+tileTheme = baseTheme
+    { -- base00, base01, blue all good activeColors
+      activeColor           = base01
+    , activeBorderColor     = base01
+    , activeTextColor       = base03
+    }
+
+altTileTheme :: Theme
+altTileTheme = baseTheme
+    { -- base00, base01, blue all good activeColors
+      activeColor           = active
+    , activeBorderColor     = active
+    , activeTextColor       = base03
+    }
 
 main = xmonad $ defaultConfig
-  { borderWidth = 2
-  , terminal    = "terminology"
-  , normalBorderColor = "#dc322f"
-  , focusedBorderColor = "#859900" }
+  { borderWidth         = 2
+  , terminal            = "terminology"
+  , normalBorderColor   = myNormalBorderColor
+  , focusedBorderColor  = myFocusedBorderColor
+  , focusFollowsMouse   = False
+  , clickJustFocuses    = False
+  }
